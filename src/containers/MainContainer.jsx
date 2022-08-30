71d707d1022ca5fd75466bb5b18a4d21314c3d8e
David <scriptura7773@gmail.com>
2022-08-30 14:53:09 -0700

    added fetch call again

--- src/containers/MainContainer.jsx
+++ src/containers/MainContainer.jsx
@@ -9,12 +9,37 @@ const MainContainer = () => {
 
   // UseEffect call
   useEffect(() => {
-    setData(beamData);
+    // fetchData();
+    // Temp fix for endpoint issue 
+    setData(beamData)
   }, []);
 
-  // Crea
+  // Fetching Data from endpoint
+  // Initialize API Url 
+
+  const dataUrl = 'https://beamtech.github.io/boxing-kata-js/perks.json';
+
+  async function fetchData () {
+    try {
+      const res = await fetch(dataUrl, {
+        headers: {
+          'Content-type': 'application/json',
+          'Accept': 'application/json'
+        },
+        mode: 'no-cors'
+      })
+      // Json the response.
+      const newData = res.json();
+      setData(newData);
+
+      // Error Handling
+    } catch (err) {
+      console.error(`Issue with FetchData ${err}`)
+    }
+  }
 
   console.log('data', data)
+
   return (
     <div className='shipping'>
       <MainData/>
