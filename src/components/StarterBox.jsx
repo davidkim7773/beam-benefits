f1396741a18aa1c3cb3101dfbb5a157fce63cc1e
David <scriptura7773@gmail.com>
2022-09-01 15:26:17 -0700

    added some more styling options

--- src/components/StarterBox.jsx
+++ src/components/StarterBox.jsx
@@ -9,7 +9,6 @@ const StarterBox = (props) => {
   // Create an array of ul components that will hold the boxes
   const brushArr = [];
 
-  console.log('data', data);
   for (const prop in colorData) {
     if (colorData[prop] <= 2) {
       brushArr.push(
@@ -26,7 +25,7 @@ const StarterBox = (props) => {
 
   return(
     <div className='starter-box'>
-      <h4><strong>Summary:</strong></h4>
+      <h4 className='summary-title'>Summary:</h4>
       <ul className='summary-starter'>
         <li>Starter Boxes: {starterBox} </li>
         <li>Brushes: {colorData.totalBrushes}</li>
